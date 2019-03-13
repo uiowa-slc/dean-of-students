ff12cdeee6867874587bb2b9f058f3d606ff6498
Dustin Quam <quamsta@gmail.com>
2019-03-13 16:04:41 -0500

    allow styles on i tags

--- mysite/_config.php
+++ mysite/_config.php
@@ -11,7 +11,7 @@ $validator->minLength(8);
 $validator->checkHistoricalPasswords(6);
 Member::set_password_validator($validator);
 HtmlEditorConfig::get('cms')->setOption(
-	'extended_valid_elements', "img[class|src|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|usemap],#iframe[src|name|width|height|align|frameborder|marginwidth|marginheight|scrolling],object[width|height|data|type],param[name|value],map[class|name|id],area[shape|coords|href|target|alt],i[class|aria-hidden]"
+	'extended_valid_elements', "img[class|src|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|usemap],#iframe[src|name|width|height|align|frameborder|marginwidth|marginheight|scrolling],object[width|height|data|type],param[name|value],map[class|name|id],area[shape|coords|href|target|alt],i[class|aria-hidden|style]"
 );
 if(Director::isLive()) {
 	Director::forceSSL();
